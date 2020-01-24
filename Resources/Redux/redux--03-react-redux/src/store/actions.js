41d40fc2138436b8d550166fb33b358009f59145
Shamin <shamin@corp.ubix.in>
2020-01-24 11:00:57 +0530

    14_Redux

--- Resources/Redux/redux--03-react-redux/src/store/actions.js
+++
@@ -1,8 +0,0 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = "DECREMENT";
-export const CHANGE = "CHANGE";
-export const ADD= "ADD";
-export const SUB = "SUB";
-export const STO_RESULTS = "STO_RESULTS";
-export const DEL_RESULTS = "DEL_RESULTS";
-
